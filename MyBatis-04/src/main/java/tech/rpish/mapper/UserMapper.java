a2968af4007b9e45ad3eacd223c4d976c481ed34
rpish <rpishmial@gmail.com>
2021-09-23 22:46:35 +0800

    Commit:MyBatis-4 mybatis与spring整合-mapperBeanFactory

---
+++ MyBatis-04/src/main/java/tech/rpish/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package tech.rpish.mapper;
+
+import tech.rpish.po.User;
+
+public interface UserMapper {
+    public User findUserById(Integer id);
+}
--- MyBatis-04/src/test/java/DaoTest.java
+++ MyBatis-04/src/test/java/DaoTest.java
@@ -2,14 +2,15 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import tech.rpish.dao.UserDao;
+import tech.rpish.mapper.UserMapper;
 import tech.rpish.po.User;
 
 public class DaoTest {
     @Test
     public void findUserByDaoTest() {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
-        User user = userDao.findUserById(1);
+        UserMapper userMapper = (UserMapper) applicationContext.getBean("userMapper");
+        User user = userMapper.findUserById(1);
         System.out.println(user);
     }
 }
